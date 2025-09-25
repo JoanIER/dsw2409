b10998c63359fa5993cb11ccfc6884399ada82e0
JoanIER <joanmanuelmarquezbrito@alumno.ieselrincon.es>
2025-09-25 12:30:44 +0100

    Archivo holamundo en PHP

---
+++ holamundo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Primer script en PHP</title>
+    </head>
+    <body>
+	<h1>
+        <?php echo '<p>Hola mundo</p>'; ?>
+	</h1>
+    </body>
+</html>
