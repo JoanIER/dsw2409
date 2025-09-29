da778c0e9e6de6a99b3518f31faadb76c7fae304
JoanIER <joanmanuelmarquezbrito@alumno.ieselrincon.es>
2025-09-29 12:00:32 +0100

    Segundo script en PHP

---
+++ holamundo2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Segundo script en PHP</title>
+    </head>
+    <body>
+	<h1>
+        <?php echo '<p>Script 2 en PHP</p>'?>
+	</h1>
+    </body>
+</html>
